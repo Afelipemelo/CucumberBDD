d904cb5bd0900dcc1f93ca4086500c6b5bd89ae7
Afelipemelo <melosantosandres@gmail.com>
2022-11-16 22:34:32 -0500

    cambio

--- features/features/is_it_friday_yet.feature
+++ features/features/is_it_friday_yet.feature
@@ -1,19 +1,32 @@
 
- Feature: agendar citas de una veterinaria
-   Este Feature es para validar el funcionamineto de un modulo para agendar citas y 
-   ver el historial 
+#  Feature: agendar citas de una veterinaria
+#    Este Feature es para validar el funcionamineto de un modulo para agendar citas y 
+#    ver el historial 
 
 
-     Scenario Outline: verificar campos del formulario
+#      Scenario Outline: verificar campos del formulario
 
-         Given El usuario
+#          Given El usuario
 
-         When Genera una cita y envia <mascota> <propietario> <fecha> <hora> <Sintomas> <estado>
+#          When Genera una cita y envia <mascota> <propietario> <fecha> <hora> <Sintomas> <estado>
 
-         Then sabe si todos los campos estan completos <estado>
+#          Then sabe si todos los campos estan completos <estado>
 
-      Examples:
-      
-        | mascota | propietario | fecha        |hora   | Sintomas |estado |
-        | REX     | pepito      | 20/10/2022   |  7:00 |n/a       |true   |
-        | pepa    |             | 20/10/2022   |  7:00 |n/a       |false  |
+#       Examples:
+
+#         | mascota | propietario | fecha        |hora   | Sintomas |estado |
+#         | 'REX'     | 'pepito'      | '20/10/2022'   |  '7:00' |'malito'       |true   |
+#         | 'pepa'    |    ''         | '20/10/2022'   |  '7:00' |'bueno'      |false  |
+Feature: Is it Friday yet?
+  Everybody wants to know when it's Friday
+
+  Scenario Outline: Today is or is not Friday
+    Given today is "<day>"
+    When I ask whether it's Friday yet
+    Then I should be told "<answer>"
+
+  Examples:
+    | day            | answer |
+    | Friday         | TGIF   |
+    | Sunday         | Nope   |
+    | anything else! | Nope   |
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,22 +1,40 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
 
-    // function validarCampos = () => {
-
+    // const validarCampos = (string, string2, string3, string4, string5) => {
+    //     if(string.trim() == "" || string2.trim() == "" || int == 0 || int2 == 0 || string3.trim() == ""){
+    //             return "false"
+    //     }else {
+    //             return "true"
+    //     }
     // }
+    // Given('El usuario', function () {
+    // // Write code here that turns the phrase above into concrete actions
+    //     return 'pending';
+    // });
 
-    Given('El usuario', function () {
-    // Write code here that turns the phrase above into concrete actions
-        return 'pending';
-    });
-
-    When('Genera una cita y envia {string} {string} {int} {int} {string}', function (string, string2, int, int2, string3) {
-        if(string.trim() == "" || string2.trim() == "" || int == 0 || int2 == 0 || string3.trim() == ""){
-            console.log('entre')
-        }else {
-            console.log('sali')
+    // When('Genera una cita y envia {string} {string} {string} {string} {string}', function (string, string2, string3, string4, string5) {
+    //   this.ejemplo =  validarCampos(string, string2, string3, string4, string5)
+    // });
+    // Then('sabe si todos los campos estan completos {string}', function (string) {
+    //     assert.strictEqual(this.actualAnswer, expectedAnswer);
+    //   });
+    function isItFriday(today) {
+        if (today === "Friday") {
+          return "TGIF";
+        } else {
+          return "Nope";
         }
-    });
-    Then('sabe si todos los campos estan completos {string}', function (string) {
-       return string;
+      }
+      
+      Given('today is {string}', function (givenDay) {
+        this.today = givenDay;
+      });
+      
+      When('I ask whether it\'s Friday yet', function () {
+        this.actualAnswer = isItFriday(this.today);
+      });
+      
+      Then('I should be told {string}', function (expectedAnswer) {
+        assert.strictEqual(this.actualAnswer, expectedAnswer);
       });
